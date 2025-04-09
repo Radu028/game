ed16eb03aad0c0626f83eccea61f4d89b67662b5
Radu <radupopa028@gmail.com>
2025-04-09 19:54:55 +0300

    feat: add GameObject class for game entity management and update VSCode configuration

---
+++ include/GameObject.h
@@ -0,0 +1,31 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "raylib.h"
+
+class GameObject {
+   protected:
+    Vector3 position;
+    Vector3 size;
+    Color color;
+    bool hasCollision;
+
+   public:
+    GameObject(Vector3 position, Vector3 size, Color color, bool hasCollision = true);
+    virtual ~GameObject() = default;
+
+    Vector3 getPosition() const { return position; }
+    Vector3 getSize() const { return size; }
+    Color getColor() const { return color; }
+    bool getHasCollision() const { return hasCollision; }
+
+    void setPosition(Vector3 newPosition) { this->position = newPosition; }
+
+    BoundingBox getBoundingBox() const;
+    virtual void update(float deltaTime) {}
+    virtual void draw() const;
+
+    bool checkCollision(const GameObject& other) const;
+};
+
+#endif
