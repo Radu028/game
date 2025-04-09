2b40fa198cbc4a4dbb762844a43e6bf362499d69
Radu <radupopa028@gmail.com>
2025-04-09 20:01:59 +0300

    feat: implement GameObject class with collision detection and rendering capabilities

---
+++ src/GameObject.cpp
@@ -0,0 +1,19 @@
+#include "GameObject.h"
+
+GameObject::GameObject(Vector3 position, Vector3 size, Color color, bool hasCollison = true)
+    : position(position), size(size), color(color), hasCollision(hasCollision) {}
+
+BoundingBox GameObject::getBoundingBox() const {
+    return BoundingBox{
+        (Vector3){position.x - size.x / 2, position.y - size.y / 2, position.z - size.z / 2},
+        (Vector3){position.x + size.x / 2, position.y + size.y / 2, position.z + size.z / 2}};
+}
+
+void GameObject::draw() const {
+    DrawCube(this->position, this->size.x, this->size.y, this->size.y, this->color);
+}
+
+bool GameObject::checkCollision(const GameObject& other) const {
+    if (!this->hasCollision || !other.getHasCollision()) return false;
+    return CheckCollisionBoxes(this->getBoundingBox(), other.getBoundingBox());
+}
